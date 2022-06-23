f4c87c8571fabf5a82a9b430b765d1d349629173
green hand <487751965@qq.com>
2022-06-23 21:26:04 +0800

    feat: complete searchpage

--- src/views/goods/SearchGoods.vue
+++ src/views/goods/SearchGoods.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="search-goods">
-    <template v-if="goodsList.length">购物</template>
+    <template v-if="goodsList.length">
+      <div class="title">为您找到以下商品</div>
+      <div class="main">
+        <SxxGoods v-for="goods in goodsList" :key="goods.id" :goods="goods" />
+      </div>
+    </template>
     <template v-else>
       <div class="empty">
         <img src="../../assets/imgs/empty-search.png" />
@@ -13,6 +18,7 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 import { getAssociateList } from "@/api";
+import SxxGoods from "@/components/SxxGoods.vue";
 import { ref } from "vue";
 import type { IGoodsRes } from "../home/types";
 
@@ -21,12 +27,25 @@ const goodsList = ref<IGoodsRes[]>([]);
 console.log(route);
 getAssociateList(route.query.searchWords as string).then((res) => {
   goodsList.value = res.data;
+  console.log(res.data);
 });
 </script>
 
 <style scoped lang="less">
 .search-goods {
-  padding: 80px;
+  padding: 70px;
+  .title {
+    background-color: #fff;
+    font-size: 20px;
+    color: @sxColor;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 10px;
+  }
+  .main {
+    display: flex;
+    flex-wrap: wrap;
+  }
   .empty {
     display: flex;
     flex-direction: column;
